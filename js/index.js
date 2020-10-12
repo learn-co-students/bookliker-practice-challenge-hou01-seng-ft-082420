cd94a68abc7e0c1a2aa72041ca80f4f033da9cec
stephenandersondev <stephenandersondev@gmail.com>
2020-10-12 18:28:01 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,83 @@
-document.addEventListener("DOMContentLoaded", function() {});
+const booksURL = "http://localhost:3000/books";
+document.addEventListener("DOMContentLoaded", function () {
+    getBooks();
+});
+
+
+function getBooks() {
+    fetch(booksURL)
+        .then(res => res.json())
+        .then(books => {
+            books.forEach(book => {
+                renderList(book);
+            })
+        })
+}
+
+function renderList(book) {
+    const list = document.getElementById("list")
+    const listItem = document.createElement("li")
+    listItem.innerText = book.title
+    listItem.addEventListener("click", (e) => {
+        renderBook(book);
+    })
+    list.append(listItem)
+}
+
+
+function renderBook(book) {
+
+    const image = document.createElement("img")
+    image.src = book.img_url
+
+    const title = document.createElement("h3")
+    title.innerText = book.title
+
+    const subTitle = document.createElement("h3")
+    subTitle.innerText = book.subtitle
+
+    const author = document.createElement("h3")
+    author.innerText = book.author
+
+    const desc = document.createElement("p")
+    desc.innerText = book.description
+
+    const userList = document.createElement("ul")
+    const users = book.users
+    const newUsers = users
+
+    const showPanel = document.getElementById("show-panel")
+    showPanel.innerHTML = ""
+
+    for (const user of users) {
+        const li = document.createElement("li")
+        li.innerText = user.username
+        userList.append(li)
+    }
+
+    const likeBtn = document.createElement("button")
+    likeBtn.innerHTML = "Like"
+    likeBtn.addEventListener("click", (e) => {
+        if (likeBtn.innerHTML == "Unlike") {
+            console.log("included")
+            likeBtn.innerHTML = "Like"
+            newUsers.pop()
+        } else {
+            console.log("not included")
+            likeBtn.innerHTML = "Unlike"
+            newUsers.push({ "id": 1, "username": "pouros" })
+        }
+        fetch(booksURL + `/${book.id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                users: newUsers
+            })
+        })
+            .then(renderBook(book))
+    })
+    showPanel.append(image, title, subTitle, author, desc, userList, likeBtn)
+}
